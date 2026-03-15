2a650eec9b9772d31085075c96b149b713df25a8
Ben Lewis <7391596+BenLewis-Seequent@users.noreply.github.com>
2026-03-16 11:33:48 +1300

    Specific CRS exception class (#186)
    <!--
    Thank you for taking the time to make a pull request.
    
    Please review our [contribution
    guide](https://github.com/SeequentEvo/evo-data-converters/blob/main/CONTRIBUTING.md)
    and our
    [code of
    conduct](https://github.com/SeequentEvo/evo-data-converters/blob/main/CONTRIBUTING.md)
    before opening your first
    pull request.
    -->
    
    ## Description
    
    <!-- Describe your proposed changes in detail -->
    
    Changed error raised by the `crs_` functions to be a custom exception
    class, so it can be caught explicitly by consumers.
    
    Also changed `convert_obj` docstring to include this exception in
    additional to `InvalidOBJError`. This makes it clear what exceptions
    that can be raised.
    
    ## Checklist
    
    - [x] I have read the contributing guide and the code of conduct

--- packages/obj/src/evo/data_converters/obj/importer/exceptions.py
+++ packages/obj/src/evo/data_converters/obj/importer/exceptions.py
@@ -1,4 +1,4 @@
-#  Copyright © 2025 Bentley Systems, Incorporated
+#  Copyright © 2026 Bentley Systems, Incorporated
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
@@ -9,10 +9,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .exceptions import InvalidOBJError
-from .obj_to_evo import convert_obj
 
-__all__ = [
-    "convert_obj",
-    "InvalidOBJError",
-]
+class InvalidOBJError(Exception):
+    """Raised when the input OBJ file is invalid or cannot be parsed."""
